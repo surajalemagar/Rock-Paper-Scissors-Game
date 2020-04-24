67e70dbbc38bf28f5a589bfe6c4b0a1fe8c7c7a7
suraj <surajido111@gmail.com>
2020-04-24 11:02:54 +0545

    functions added for simplicity and backup and test file is deleted

--- rpsgame.py
+++ rpsgame.py
@@ -1,32 +1,40 @@
 import random
 
-def Result(player_choice,computer_choice):
-    if player_choice==1:
-        if  computer_choice==1:
-            print("Your choice is Rock and Comuter choice is also Rock.\nMatch Tie!!!")    
-        elif computer_choice==2:
-            print("Your choice is Rock and Comuter choice is Paper.\nYou Lose the game!!!")    
-        else:
-            print("Your choice is Rock and Comuter choice is Scissors.\nYou Win the game!!!") 
-
-    elif player_choice==2:
-        if computer_choice==1:
-            print("Your choice is Paper and Comuter choice is Rock.\nYou Win the game!!!")   
-        elif computer_choice==2:
-            print("Your choice is Paper and Comuter choice is also Paper.\nMatch Tie!!!")    
-        else:
-            print("Your choice is Paper and Comuter choice is Scissors.\nYou Lose the game!!!")
-            
+def gorock(computer_choice):
+    if  computer_choice==1:
+        print("\nYour choice is Rock and Computer choice is also Rock.\nMatch Tie!!!")    
+    elif computer_choice==2:
+        print("\nYour choice is Rock and Computer choice is Paper.\nYou Lose the game!!!")    
     else:
-        if computer_choice==1:
-            print("Your choice is Scissors and Comuter choice is  Rock.\nYou Lose the game!!!")   
-        elif computer_choice==2:
-            print("Your choice is Scissors and Comuter choice is Paper.\nYou Win the game!!!")  
-        else:
-            print("Your choice is Scissors and Comuter choice is also Scissors.\nMatch Tie!!!")
+        print("\nYour choice is Rock and Computer choice is Scissors.\nYou Win the game!!!") 
 
+def gopaper(computer_choice):
+    if computer_choice==1:
+        print("\nYour choice is Paper and Computer choice is Rock.\nYou Win the game!!!")   
+    elif computer_choice==2:
+        print("\nYour choice is Paper and Computer choice is also Paper.\nMatch Tie!!!")    
+    else:
+        print("\nYour choice is Paper and Computer choice is Scissors.\nYou Lose the game!!!")
 
-print("Player Choice!!!")
-player_choice=int(input("Enter 1 for Rock,2 for Paper and 3 for Scissors:"))
-computer_choice=random.randint(1,3)
-Result(player_choice,computer_choice)
+def goscissors(computer_choice):
+    if computer_choice==1:
+        print("\nYour choice is Scissors and Computer choice is  Rock.\nYou Lose the game!!!")   
+    elif computer_choice==2:
+        print("\nYour choice is Scissors and Computer choice is Paper.\nYou Win the game!!!")  
+    else:
+        print("\nYour choice is Scissors and Computer choice is also Scissors.\nMatch Tie!!!")
+
+c='y'  
+print("Welcome To Rock Paper And Scissors Game!!!")    
+while c=='y' or c=='Y':
+    print("\nYour Choice!!!")
+    player_choice=int(input("Enter 1 for Rock,2 for Paper and 3 for Scissors:"))
+    computer_choice=random.randint(1,3)
+    if player_choice==1:
+        gorock(computer_choice)
+    elif player_choice==2:
+        gopaper(computer_choice)
+    else:
+        goscissors(computer_choice)
+    c=input("Do You Want To Play Again??(y/n)....")
+print("\nThank You For Playing This Beautiful Game!!!")
